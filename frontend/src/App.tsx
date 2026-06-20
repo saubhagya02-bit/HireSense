ce4234c88fad2a96394e40e9f8854543171f3f64
saubhagya02-bit <saubhagyaushani03@gmail.com>
2026-06-20 07:05:21 +0530

    feat(router): add routes for forgot password, reset password and settings pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,16 +2,18 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import { useAuthStore } from "./store/authStore";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
+import ForgotPasswordPage from "./pages/ForgotPasswordPage";
+import ResetPasswordPage from "./pages/ResetPasswordPage";
 import DashboardPage from "./pages/DashboardPage";
 import InterviewSetupPage from "./pages/InterviewSetupPage";
 import InterviewPage from "./pages/InterviewPage";
 import ResultsPage from "./pages/ResultsPage";
 import ResumePage from "./pages/ResumePage";
+import SettingsPage from "./pages/SettingsPage";
 import AnalyticsPage from "./pages/AnalyticsPage";
 import Layout from "./components/common/Layout";
 
-function PrivateRoute({ children }: { children: React.ReactNode })
-{
+function PrivateRoute({ children }: { children: React.ReactNode }) {
   const token = useAuthStore((s) => s.token);
   return token ? <>{children}</> : <Navigate to="/login" replace />;
 }
@@ -19,8 +21,13 @@ function PrivateRoute({ children }: { children: React.ReactNode })
 export default function App() {
   return (
     <Routes>
+      {/* Public */}
       <Route path="/login" element={<LoginPage />} />
       <Route path="/register" element={<RegisterPage />} />
+      <Route path="/forgot-password" element={<ForgotPasswordPage />} />
+      <Route path="/reset-password" element={<ResetPasswordPage />} />
+
+      {/* Protected */}
       <Route
         path="/"
         element={
@@ -28,14 +35,15 @@ export default function App() {
             <Layout />
           </PrivateRoute>
         }
-        >
-          <Route index element={<DashboardPage />} />
-          <Route path="interview/new" element={<InterviewSetupPage />} />
-          <Route path="interview/:id" element={<InterviewPage />} />
-          <Route path="interview/:id/results" element={<ResultsPage />} />
-          <Route path="resume" element={<ResumePage />} />
-          <Route path="analytics" element={<AnalyticsPage />} />
-        </Route>
+      >
+        <Route index element={<DashboardPage />} />
+        <Route path="interview/new" element={<InterviewSetupPage />} />
+        <Route path="interview/:id" element={<InterviewPage />} />
+        <Route path="interview/:id/results" element={<ResultsPage />} />
+        <Route path="resume" element={<ResumePage />} />
+        <Route path="analytics" element={<AnalyticsPage />} />
+        <Route path="settings" element={<SettingsPage />} />
+      </Route>
     </Routes>
   );
 }
